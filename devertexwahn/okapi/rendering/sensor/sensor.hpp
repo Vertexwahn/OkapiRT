7edb7f708f96438e2464149e4692b2a343758e58
Vertexwahn <julian.amann@tum.de>
2026-03-05 01:12:14 +0100

    Update sensor.hpp

--- devertexwahn/okapi/rendering/sensor/sensor.hpp
+++ devertexwahn/okapi/rendering/sensor/sensor.hpp
@@ -13,6 +13,8 @@
 #include "okapi/rendering/sensor/film.hpp"
 #include "core/exception.hpp"
 
+#include "boost/algorithm/string.hpp"
+
 #include <iostream>
 #include <string>
 
@@ -27,11 +29,53 @@ public:
     using Scalar = ScalarType;
     using Transform = Transform44Type<ScalarType>;
 
+    ScalarType parse_fov(const PropertySet &props, double aspect) {
+        if (props.has_property("fov") && props.has_property("focal_length"))
+            throw Exception("Please specify either a focal length ('focal_length') or a "
+                "field of view ('fov')!");
+
+        double fov;
+        std::string fov_axis;
+
+        if (props.has_property("fov")) {
+            fov = props.get_property<float>("fov");
+
+            fov_axis = boost::algorithm::to_lower_copy(props.get_property<std::string>("fov_axis", "x"));
+
+            if (fov_axis == "smaller")
+                fov_axis = aspect > 1 ? "y" : "x";
+            else if (fov_axis == "larger")
+                fov_axis = aspect > 1 ? "x" : "y";
+        }
+
+        ScalarType result;
+
+        if (fov_axis == "x") {
+            result = fov;
+        } else if(fov_axis == "y") {
+            result = radian_to_degree((
+                2.0 * std::atan(std::tan(0.5 * degree_to_radian(fov)) * aspect)));
+        }
+        else {
+            throw std::runtime_error("Unsupported fov axis");
+        }
+
+        if (result <= 0.0 || result >= 180.0)
+            throw Exception("The horizontal field of view must be in the range [0, 180]!");
+
+        return degree_to_radian(result);
+    }
+
     SensorType(const PropertySet& ps) : SensorTypeBase<ScalarType, 3>(ps) {
         film_ = std::dynamic_pointer_cast<Film>(ps.get_property<ReferenceCounted<Object>>("film"));
         assert(film_);
 
-        horizontal_fov_ = degree_to_radian(ps.get_property<float>("fov", 30.0f));
+        //horizontal_fov_ = degree_to_radian(ps.get_property<float>("fov", 30.0f));
+        Vector2<ScalarType> film_size_f = Vector2f(SensorType<ScalarType, 3>::film_->size().x(),
+                                                   SensorType<ScalarType, 3>::film_->size().y());
+        Scalar aspect = film_size_f.x() / film_size_f.y();
+        horizontal_fov_ = parse_fov(ps, aspect);
+
         near_clip_plane_distance_ = ps.get_property<float>("near_clip", 0.1f);
         far_clip_plane_distance_ = ps.get_property<float>("far_clip", 500.0f);
         // TODO: does mitsuba use focus_distance_?
@@ -47,23 +91,9 @@ public:
 
         perspective_ = perspective(horizontal_fov_, near_clip_plane_distance_, far_clip_plane_distance_);
 
-        Vector2<ScalarType> film_size_f = Vector2f(SensorType<ScalarType, 3>::film_->size().x(),
-                                                   SensorType<ScalarType, 3>::film_->size().y());
-
         auto rasterSpaceToNDC = raster_space_to_ndc(film_size_f);
 
-        Scalar aspect = film_size_f.x() / film_size_f.y();
-/*
-        std::string fov_axis = ps.get_property<std::string>("fov_axis", "x");
-        if (fov_axis == "smaller")
-            fov_axis = aspect > 1 ? "y" : "x";
 
-        if (fov_axis == "y") {
-            result = dr::rad_to_deg(
-                    2.0 * dr::atan(dr::tan(0.5 * dr::deg_to_rad(fov)) * aspect));
-        }
-
-        */
         raster_space_to_camera_ = perspective_.inverse() * scale(1.f, 1.f / aspect, 1.f) * rasterSpaceToNDC;
     }
 
